756d76338e5e59a5f59359b11b9b1623afcf9e96
Noahh93 <Noaf_33@hotmail.com>
2023-01-29 14:56:19 +0100

    Added BusinessLayer Project

---
+++ BusinessLayer/BasicMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class BasicMath //Classes are Internal by default
+    {
+        public int Add(int x, int y)   //Class members are by default private
+        {
+            int SUM1 = x + y;
+            return SUM1;
+        }
+        public int Multiply(int k, int l)
+        {
+            int SUM2 = k * l;
+            return SUM2;
+        }
+    }
+}
