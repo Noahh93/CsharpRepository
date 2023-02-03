6fb306ce6c3e86cbadee446188f0ab3bbb5cedbd
Noahh93 <Noaf_33@hotmail.com>
2023-02-03 11:12:53 +0100

    Interfaces added

---
+++ Amazon/Example/Inheritance/FullTimeEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon.Example.Inheritance
+{
+    internal class FullTimeEmployee : Amazon.examples.Inheritance.Employee
+    {
+        //"sealed" keyword means that no childClass can inherite from that method
+        public override void EmployeeName(string FirstName, string LastName)  
+        {
+            Console.WriteLine($"This employees name is :{FirstName}, {LastName} Fulltime");
+        }
+
+
+
+
+    }
+
+    
+}
